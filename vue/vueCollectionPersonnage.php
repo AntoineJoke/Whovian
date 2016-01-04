909948de7b622366ccedcbc5b2980ab9267a98d1
qurichard1 <quentin-le-gaulois@hotmail.fr>
2016-01-04 21:57:02 +0100

    Deplacement ajout docteur en bas du tableau

--- vue/vueCollectionPersonnage.php
+++ vue/vueCollectionPersonnage.php
@@ -2,16 +2,15 @@
 
 <?php
 	echo "<table><caption>Tous les Docteurs</caption><tbody>";
-		echo"<tr>";
-		echo"<td colspan=\"3\"><a href=\"?action=saisie\">ajouter un nouveau docteur</a></td>";
-		echo"<tr>";
 	foreach ($modele->getData() as $personnage) {
 		echo"<tr>";
 		echo"<td><a href=\"?action=get&id=".$personnage->getId()."\">".PersonnageView::getHtmlCompact($personnage)."</a></td>";
 		echo"<td><a href=\"?action=edit&id=".$personnage->getId()."\">modifier</a></td>";
 		echo"<td><a href=\"?action=delete&id=".$personnage->getId()."\">supprimer</a></td>";
+
 		echo"<tr>";
 	}
+	echo"<td colspan=\"3\"><a href=\"?action=saisie\">ajouter un nouveau docteur</a></td>";
 	echo"</tbody></table>";
 ?>
 <?=VueHtmlUtils::finFichierHTML5()?>
